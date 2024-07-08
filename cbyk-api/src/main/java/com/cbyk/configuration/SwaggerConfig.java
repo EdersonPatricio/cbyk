eb32a29130d6e532118dd7a3682d50dab897891c
Ederson Souza <esouza@cleartech.com.br>
2024-07-07 23:30:13 -0300

    -  Retificação.

--- cbyk-api/src/main/java/com/cbyk/configuration/SwaggerConfig.java
+++ cbyk-api/src/main/java/com/cbyk/configuration/SwaggerConfig.java
@@ -17,24 +17,17 @@ public class SwaggerConfig {
 	@Bean
 	OpenAPI openAPI() {
 		return new OpenAPI()
-	        .info( new Info()
-                .title( "SPRING REST API" )
-                .description( "Documentação da API REST" )
-                .version( "1.0" )
-                .termsOfService( "Termo de uso: Open Source" )
-				.license( 
-					new License()
-					.name( "Apache 2.0" )
-					.url( "http://localhost" ) 
-				)
-			).externalDocs(
-			        new ExternalDocumentation()
-	                .description( "Ederson Patrício" )
-				                .url( "http://localhost" ) 
-					)
+	        .info( new Info().title( "SPRING REST API" )
+        		.description( "Documentação da API REST" )
+        		.version( "1.0" )
+        		.termsOfService( "Termo de uso: Open Source" )
+        		.license( new License().name( "Apache 2.0" ).url( "http://localhost" ) ) )
+	        .externalDocs( new ExternalDocumentation()
+        		.description( "Ederson Patrício" ).url( "http://localhost" )
+    		)
 	        .components( new Components()
-                .addSecuritySchemes( "basicScheme", new SecurityScheme()
-                .type( SecurityScheme.Type.HTTP ).scheme( "basic" ) ) )
+        		.addSecuritySchemes( "basicScheme", new SecurityScheme().type( SecurityScheme.Type.HTTP ).scheme( "basic" ) )
+	        )
 	        .addSecurityItem( new SecurityRequirement().addList( "basicScheme" ) );
 	}
 }
